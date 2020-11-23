0d1fde049b28080d2c38ea56eb39001ef4c533ed
Vivekyadv <vivek.yadav2611@gmail.com>
2020-11-23 21:35:21 +0530

    Tut2 complete

--- chatterpro/settings.py
+++ chatterpro/settings.py
@@ -63,6 +63,16 @@ TEMPLATES = [
 WSGI_APPLICATION = 'chatterpro.wsgi.application'
 ASGI_APPLICATION = 'chatterpro.asgi.application'
 
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
+
+
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
